5e96e4e8eeb906fd1d27b07b304cfadba51f80ae
Pradyumna Mishra <pradyumna.mishra@definelabs.com>
2024-02-15 15:55:51 +0530

    edit article feature

--- db/migrate/20240215060232_create_articles.rb
+++ db/migrate/20240215060232_create_articles.rb
@@ -1,10 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
-    
+    #for change in this table structure just create another migration file 
     create_table :articles do |t|
       t.string :title
       t.text :description
     end
-
   end
 end
