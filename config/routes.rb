ebfcd968333b8060bb07edf69888e3f5c1dbb981
Pradyumna Mishra <pradyumna.mishra@definelabs.com>
2024-02-19 14:07:40 +0530

    add sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'about', to:'pages#about'
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update]  "used for specific article operations"
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
