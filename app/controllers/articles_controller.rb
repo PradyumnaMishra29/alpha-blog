a061a278d7d00bc4e2c42e47e493decd2fef127a
Pradyumna Mishra <pradyumna.mishra@definelabs.com>
2024-02-20 17:20:52 +0530

    add delete user with associated articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:show, :edit, :update, :destroy]
+	before_action :require_user, except: [:show, :index]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 	
 	def show
 		#byebug
@@ -44,8 +46,7 @@ class ArticlesController < ApplicationController
 		redirect_to articles_path
 	end
 
-	private 
-
+	private
 	def set_article
 		@article = Article.find(params[:id])
 	end
@@ -54,4 +55,10 @@ class ArticlesController < ApplicationController
 		params.require(:article).permit(:title, :description)
 	end
 
+	def require_same_user
+		if current_user != @article.user
+			flash[:alert] = "You can only edit or delete your own article"
+			redirect_to @article
+		end
+	end
 end
